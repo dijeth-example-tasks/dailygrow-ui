5852a37963c6d52e6019a0ca2d4a30c7b2f6f0dc
Dmitry Orlov <d.orlov777@gmail.com>
2024-03-11 14:17:28 +0300

    fix: disable task while editing

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -12,7 +12,7 @@
         </el-popover>
       </template>
     </el-table-column>
-    <el-table-column label="Вид">
+    <el-table-column label="Вид" width="80">
       <template #default="scope">
         {{ scope.row.type }}
       </template>
@@ -25,18 +25,19 @@
         </div>
       </template>
     </el-table-column>
-    <el-table-column label="Статус">
+    <el-table-column label="Статус" width="120">
       <template #default="scope">
         <el-text v-if="scope.row.active" type="success">Активна</el-text>
         <el-text v-else type="info">Неактивна</el-text>
       </template>
     </el-table-column>
-    <el-table-column label="Действие">
+    <el-table-column label="Действие" width="120">
       <template #default="scope">
         <el-button
           v-if="!scope.row.active"
           type="success"
           size="small"
+          :disabled="disabledTasks.includes(scope.row.id)"
           @click="handleEdit({ ...scope.row, active: true })"
           >Запустить</el-button
         >
@@ -44,14 +45,21 @@
           v-else
           type="danger"
           size="small"
+          :disabled="disabledTasks.includes(scope.row.id)"
           @click="handleEdit({ ...scope.row, active: false })"
           >Остановить</el-button
         >
       </template>
     </el-table-column>
-    <el-table-column label="Удаление">
+    <el-table-column label="Удаление" width="120">
       <template #default="scope">
-        <el-button type="danger" size="small" @click="handleDelete(scope.row)">Удалить</el-button>
+        <el-button
+          type="danger"
+          size="small"
+          @click="handleDelete(scope.row)"
+          :disabled="disabledTasks.includes(scope.row.id)"
+          >Удалить</el-button
+        >
       </template>
     </el-table-column>
   </el-table>
@@ -66,6 +74,7 @@ import { ref, watch } from 'vue'
 
 const props = defineProps<{ tasks: TTask[] }>()
 const taskList = ref<TTask[]>()
+const disabledTasks = ref<string[]>([])
 
 watch(
   () => props.tasks,
@@ -75,13 +84,17 @@ watch(
 )
 
 const handleEdit = async (task: TTask) => {
+  disabledTasks.value.push(task.id)
   await updateTask(task)
   const taskIndex = taskList.value!.findIndex(({ id }) => task.id === id)
   taskList.value![taskIndex] = task
+  disabledTasks.value = disabledTasks.value.filter((id) => id !== task.id)
 }
 
 const handleDelete = async (task: TTask) => {
+  disabledTasks.value.push(task.id)
   await deleteTask(task)
   taskList.value = taskList.value!.filter(({ id }) => task.id !== id)
+  disabledTasks.value = disabledTasks.value.filter((id) => id !== task.id)
 }
 </script>
